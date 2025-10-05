5deb610bc7da2d270906168578555cba4e3eaa55
HP <HP@User>
2025-10-05 11:25:15 +0530

    commit

---
+++ COREJAVA_PROGRAMS/src/DAY8/Interfaces/nestedinterfaces/MyInterface.java
@@ -0,0 +1,9 @@
+package DAY8.Interfaces.nestedinterfaces;
+
+public interface MyInterface {
+	 void calculateArea();
+	    interface MyInnerInterface {
+	       int  id = 20;
+	       void print();     
+	    }
+}
