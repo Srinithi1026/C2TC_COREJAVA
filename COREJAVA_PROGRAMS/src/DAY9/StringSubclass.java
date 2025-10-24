27a90f7f77f5da393d7697e80c704e0ff0b3f309
HP <HP@User>
2025-10-24 19:45:40 +0530

    commit

---
+++ COREJAVA_PROGRAMS/src/DAY9/StringSubclass.java
@@ -0,0 +1,8 @@
+package DAY9;
+public class StringSubclass extends Strings {
+
+	public static void main(String[] args) {
+		
+
+	}
+}
---
+++ COREJAVA_PROGRAMS/src/DAY9/Strings.java
@@ -0,0 +1,10 @@
+package DAY9;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
