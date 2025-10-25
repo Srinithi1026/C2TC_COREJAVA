d8cd1ca2ec042e78ef33112ea6180b7d9a21a9fd
HP <HP@User>
2025-10-25 12:46:15 +0530

    commit

---
+++ COREJAVA_PROGRAMS/src/DAY11/v1/Demo.java
@@ -0,0 +1,11 @@
+package DAY11.v1;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("the program continues....");
+		int data=100/0;// TODO Auto-generated method stub
+
+	}
+
+}
